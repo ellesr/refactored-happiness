0a2820951ff8331ec154f8a674d68145075bdebd
ellesr <116410947+ellesr@users.noreply.github.com>
2022-10-21 18:04:28 -0700

    Add files via upload

---
+++ Rukavina.A3.py
@@ -0,0 +1,4 @@
+l = int(input('Enter the length of the rectangle: '))
+w = int(input('Enter the width of the rectangle: '))
+res = l * w
+print('The area of the rectangle is', res)
---
+++ cr415test.py
@@ -0,0 +1,44 @@
+# This program calculates a salesperson's pay
+# at Make Your Own Music.
+def main():
+    sales = get_sales()
+    advanced_pay = get_advanced_pay()
+    comm_rate = determine_comm_rate(sales)
+    pay = sales * comm_rate - advanced_pay
+    print('The pay is $', format(pay, '.2f'), sep='')
+    if pay < 0:
+        print('The Salesperson must reimburse')
+        print('the company.')
+
+# The get_sales function gets a salesperson's
+# monthly sales from the user and returns that value.
+def get_sales():
+    monthly_sales = float(input('Enter the monthly sales: '))
+    return monthly_sales
+
+# The get_advanced_pay function gets the amount of
+# advanced pay given to the salesperson and returns
+# that amount.
+def get_advanced_pay():
+    print('Enter the amount of advanced pay, or')
+    print('enter 0 if no advanced pay was given.')
+    advanced = float(input('Advanced pay: '))
+    return advanced
+
+# The determine_comm_rate function accepts the
+# amount of sales as an argument and returns the
+# applicable commission rate.
+def determine_comm_rate(sales):
+    if sales < 10000.00:
+        rate = 0.10
+    elif sales >= 10000 and sales <= 14999.99:
+        rate = 0.12
+    elif sales >= 15000 and sales <= 17999.99:
+        rate = 0.14
+    elif sales >= 18000 and sales <= 21999.99:
+        rate = 0.16
+    else:
+        rate = 0.18
+    return rate
+
+main()
